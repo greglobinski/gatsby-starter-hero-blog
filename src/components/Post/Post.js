f9af0760dea974353db7ef9e8cb2a9f2280a8a7d
greglobinski <greglobinski@gmail.com>
2018-05-17 02:17:43 +0200

    Change style import statement

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-require("prismjs/themes/prism-okaidia.css");
+import "prismjs/themes/prism-okaidia.css";
 
 import asyncComponent from "../AsyncComponent";
 import Headline from "../Main/Headline";
