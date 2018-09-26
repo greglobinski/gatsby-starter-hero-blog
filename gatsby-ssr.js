7c40b79381c96dd15fd92bf6caede8ebc6710462
greglobinski <greglobinski@gmail.com>
2018-09-26 22:45:13 +0200

    Back again to plugins

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-const flush = require("styled-jsx/server");
-
-exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  if (process.env.NODE_ENV === `production`) {
-    const css = flush();
-
-    setHeadComponents([css]);
-  }
-};
