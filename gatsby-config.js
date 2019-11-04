9fb2bcf81e3976d737c3fb464a7272e59f4d3bb9
Damian <damian.mcnamara@gmail.com>
2019-11-04 21:33:59 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,16 +62,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.ALGOLIA_APP_ID ? process.env.ALGOLIA_APP_ID : "",
-        apiKey: process.env.ALGOLIA_ADMIN_API_KEY ? process.env.ALGOLIA_ADMIN_API_KEY : "",
-        indexName: process.env.ALGOLIA_INDEX_NAME ? process.env.ALGOLIA_INDEX_NAME : "",
-        queries,
-        chunkSize: 10000 // default: 1000
-      }
-    },
+   
     {
       resolve: `gatsby-source-filesystem`,
       options: {
